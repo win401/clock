0181112b5d744b5f2a6837e48bb6951a5a658fbd
win401 <winnerv401@gmail.com>
2026-01-04 22:24:42 +0900

    add css, js

---
+++ main.js
@@ -0,0 +1,12 @@
+function updateClock() {
+  const now = new Date();
+
+  const h = String(now.getHours()).padStart(2, "0");
+  const m = String(now.getMinutes()).padStart(2, "0");
+  const s = String(now.getSeconds()).padStart(2, "0");
+
+  document.getElementById("clock").textContent = `$(h):${m}:${s}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
