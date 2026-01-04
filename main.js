224e4a0d371123c4042e4058c4652546479ed8a7
win401 <winnerv401@gmail.com>
2026-01-04 22:25:40 +0900

    fix code

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function updateClock() {
   const m = String(now.getMinutes()).padStart(2, "0");
   const s = String(now.getSeconds()).padStart(2, "0");
 
-  document.getElementById("clock").textContent = `$(h):${m}:${s}`;
+  document.getElementById("clock").textContent = `${h}:${m}:${s}`;
 }
 
 setInterval(updateClock, 1000);
